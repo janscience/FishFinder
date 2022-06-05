b1b74381b4cd84fa5f4ecd2013efe952715ec7cd
Jan Benda <jan.benda@uni-tuebingen.de>
2022-06-05 11:32:49 +0200

    [AudioMonitor] added play() and pause()

--- src/AudioMonitor.cpp
+++ src/AudioMonitor.cpp
@@ -15,6 +15,8 @@ AudioMonitor::AudioMonitor(DataWorker &data, AudioStream &speaker) :
     Beep[i] = 0;
     BeepInterval[i] = 0;
   }
+  Play = false;
+  AmplifierPin = -1;
 }
 
 
@@ -33,10 +35,24 @@ void AudioMonitor::setup(int amplifier_pin, float volume,
     VolumeButtons = true;
   }
   if ( amplifier_pin >= 0 ) {
-    pinMode(amplifier_pin, OUTPUT);
-    digitalWrite(amplifier_pin, HIGH); // turn on the amplifier
-    delay(10);                         // allow time to wake up
+    AmplifierPin = amplifier_pin;
+    pinMode(AmplifierPin, OUTPUT);
+    digitalWrite(AmplifierPin, HIGH); // turn on the amplifier
+    delay(10);                        // allow time to wake up
   }
+  Play = true;
+}
+
+
+void AudioMonitor::pause() {
+  Play = false;
+  Data.setMute(true);
+}
+
+
+void AudioMonitor::play() {
+  Play = true;
+  Data.setMute(false);
 }
 
 
@@ -47,9 +63,10 @@ void AudioMonitor::addFeedback(float gain, float freq, float duration) {
   if (Gains[0] < 0.1)
     Gains[0] = 0.1;
   Gains[NSounds+1] = gain;
-  // make a beep:
+  // generate beep waveform:
   size_t np = size_t(AUDIO_SAMPLE_RATE_EXACT/freq);
-  unsigned int n = (unsigned int)(duration*AUDIO_SAMPLE_RATE_EXACT/np)*np;
+  unsigned int n = (unsigned int)(duration*AUDIO_SAMPLE_RATE_EXACT);
+  //  unsigned int n = (unsigned int)(duration*AUDIO_SAMPLE_RATE_EXACT/np)*np;
   Beep[NSounds] = new int16_t[2+n];
   // first integer encodes format and size:
   unsigned int format = 0x81;
@@ -60,7 +77,8 @@ void AudioMonitor::addFeedback(float gain, float freq, float duration) {
   // Gabor sine tone:
   uint16_t a = 1 << 15;
   for (size_t i=0; i<n; i++)
-    Beep[NSounds][2+i] = (int16_t)(a*exp(-0.5*pow((i-n/2)/(n/4), 2))*sin(TWO_PI*i/np));
+    Beep[NSounds][2+i] = (int16_t)(a*0.5*(1.0-cos(TWO_PI*i/n))*sin(TWO_PI*i/np));
+    // Beep[NSounds][2+i] = (int16_t)(a*exp(-0.5*pow((i-n/2)/(n/4), 2))*sin(TWO_PI*i/np));
   
   NSounds++;
   setVolume();  
@@ -89,12 +107,16 @@ void AudioMonitor::volumeDown() {
 
 
 void AudioMonitor::setVolume() {
-  for (size_t i=0; i<4; i++)
-    Mixer->gain(i, Gains[i]*Volume);
+  if (Play) {
+    for (size_t i=0; i<4; i++)
+      Mixer->gain(i, Gains[i]*Volume);
+  }
 }
 
 
 void AudioMonitor::update() {
+  if (!Play)
+    return;
   if (VolumeButtons) {
     VolumeUpButton.update();
     VolumeDownButton.update();
@@ -104,7 +126,7 @@ void AudioMonitor::update() {
       volumeDown();
   }
   for (size_t i=0; i<3; i++) {
-    if (BeepInterval[i] > 0 && Beep[i] != 0 && BeepTime[i] > BeepInterval[i]) {
+    if (Beep[i] != 0 && BeepInterval[i] > 0 && BeepTime[i] > BeepInterval[i]) {
       BeepTime[i] = 0;
       Sound[i]->play((const unsigned int *)Beep[i]);
     }
