7ec7c3da1ac0ac5f7ee572240c239533110e5062
Jan Benda <jan.benda@uni-tuebingen.de>
2023-02-05 23:17:09 +0100

    added plot script for illustrating fishfinder measurement problem

---
+++ docs/images/eodcircuit.py
@@ -0,0 +1,164 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.transforms as mpt
+from matplotlib.patches import Rectangle
+from plotstyle import plot_style, spines_params
+from thunderfish.efield import efish_monopoles, epotential_meshgrid, squareroot_transform
+from thunderfish.efield import fieldline, plot_fieldlines
+from thunderfish.efield import epotential, efield
+from thunderfish.fishshapes import plot_fish
+
+
+def plot_efish(ax, s):
+    scale = 14.0
+    miny = -1.3*scale
+    maxy = scale
+    minx = -0.5*scale
+    maxx = minx + (maxy-miny)/ax.aspect_ratio()
+    x = np.linspace(minx, maxx, 13*int(maxx))
+    y = np.linspace(miny, maxy, 10*int(maxy))
+    xx, yy = np.meshgrid(x, y)
+    fish = (('Eigenmannia', 'side'), (0, 2), (0, 1), 18.0, 0)
+    poles = efish_monopoles(*fish[1:])
+    pot = epotential_meshgrid(xx, yy, None, poles)
+    mz = 0.9
+    zz = squareroot_transform(pot/200, mz)
+    levels = np.linspace(-mz, mz, 25)
+    ax.show_spines('')
+    ax.contourf(x, y, -zz, levels, zorder=0)
+    ax.contour(x, y, -zz, levels, zorder=1, **s.csLine)
+    """
+    # field lines:
+    filename = 'eodcircuit-fieldlines.npz'
+    if not os.path.isfile(filename):
+        # compute:
+        print('compute field lines:')
+        bounds = [[minx, miny], [maxx, maxy]]
+        flines = {}
+        for x0, y0 in ((minx+1, 0.2*maxy), (0.7*minx, maxy), (-5, 12), (-5, 6),
+                       (0.3*maxx, maxy), (0.8*maxx, maxy), (maxx-0.5, 0.3*maxy)):
+            fl = fieldline((x0, y0), bounds, poles, eps=0.2, maxiter=5000)
+            flines['left%02d' % len(flines)] = fl
+            print('%4.0f, %4.0f: %d' % (x0, y0, len(fl)))
+        np.savez(filename, **flines)
+    # plot field lines:
+    data = np.load(filename)
+    flines = []
+    for d in data.files:
+        dd = data[d]
+        flines.append(dd)
+    plot_fieldlines(ax, flines, 0.3*maxx, **s.lsFieldline)
+    flines = []
+    for d in data.files:
+        dd = data[d]
+        dd[:,1] *= -1
+        flines.append(dd)
+    plot_fieldlines(ax, flines, 0.3*maxx, **s.lsFieldline)
+    """
+    # fish:
+    plot_fish(ax, *fish, bodykwargs=s.fishLightBody,
+              finkwargs=s.fishTransparentFins,
+              eyekwargs=s.fishEyeLine)
+    ax.set_xlim(minx, maxx)
+    ax.set_ylim(miny, maxy)
+
+    
+def plot_fishfinder(ax, s, pos, direction, length):
+    width = 6
+    transform = mpt.Affine2D().rotate(np.arctan2(direction[1], direction[0])).translate(*pos) + ax.transData
+    ax.plot([0, length], [0, 0], color=s.palette['gray'], lw=width,
+            transform=transform, solid_capstyle='butt', zorder=50)
+    ax.plot([1, 2], [0, 0], color=s.palette['blue'], lw=1.3*width,
+            transform=transform, solid_capstyle='butt', zorder=52)
+    ax.plot([length-1, length-2], [0, 0], color=s.palette['red'], lw=1.3*width,
+            solid_capstyle='butt', transform=transform, zorder=52)
+    #ax.plot([length-2, 0], [0, 0], color=s.palette['red'], lw=s.lwthin,
+    #        solid_capstyle='butt', transform=transform, zorder=51)
+    #ax.plot([2, 0], [-0.2, -0.2], color=s.palette['blue'], lw=s.lwthin,
+    #        solid_capstyle='butt', transform=transform, zorder=51)
+
+    
+def plot_eodcircuit(ax):
+    rf1, rf2 = ax.resistance_v((0, 3.5), r'$R_f$')
+    ef1, ef2 = ax.battery_v(rf1.downs(1), r'$E_f$')
+    rw1, rw2 = ax.resistance_v(rf1.lefts(1.5), r'$R_w$')
+    eod1 = ax.node((ef1.downs(1)))
+    eod2 = ax.node(rf2.ups(1))
+    ax.connect((ef2, rf1, None, rf2, eod2, rw2, None, rw1, eod1, ef1))
+    return eod1, eod2
+
+    
+def plot_fishfinder_circuit(ax, s, eod1, eod2):
+    rm1, rm2 = ax.resistance_h(eod1.rights(5.5), r'$R_{m1}$')
+    rm3, rm4 = ax.resistance_h(eod2.rights(5.5), r'$R_{m2}$')
+    n1 = ax.node(rm2.rights(2))
+    n2 = ax.node(rm4.rights(2))
+    oppos = n1.rights(3).ups(2)
+    opn, opp, opout, opgnd = ax.opamp_l(oppos)
+    ax.connect((eod2, rm3, None, rm4, n2, opp, None, opout, opout.rights(0.5)))
+    ax.connect((opn, n1, rm2, None, rm1, eod1))
+    ngnd = ax.node(oppos.downs(5))
+    gnd = ax.ground(ngnd.downs(1))
+    ax.connect((opgnd, gnd))
+    b1, b2 = ax.battery_v(ngnd.rights(1).ups(1))
+    ax.connect((ngnd, b1))
+    plot_fishfinder(ax, s, n1.downs(0.5), (0, 1), 15.4)
+    ax.text(n1.lefts(0.8).x(), opout.y(), 'electrodes',
+            rotation='vertical', va='center')
+    ax.add_patch(Rectangle(opp.lefts(1).ups(0.7), 10.5, -22.3,
+                           zorder=50, edgecolor=s.palette['black'],
+                           facecolor=s.palette['gray']))
+    ax.text(b2.x(), b2.ups(0.5).y(), 'recorder',
+            rotation='vertical', zorder=60)
+    return ngnd, opn
+
+    
+def plot_farground(ax, eod1, eod2, ngnd):
+    n1 = ax.node(eod1.rights(2))
+    n2 = ax.node(eod2.rights(4))
+    rg1, rg2 = ax.resistance_v(n1.downs(1.5), r'$R_{g1}$')
+    rg3, rg4 = ax.resistance_v(n1.downs(1.5).rights(2), r'$R_{g2}$')
+    n3 = ax.node(rg3.downs(1))
+    ax.connect((n1, rg2, None, rg1, ngnd))
+    ax.connect((n2, rg4, None, rg3, n3))
+    ax.plot([n3.rights(0.5).x(), ngnd.x()], [n3.y(), n3.y()],
+            s.palette['gray'], lw=4, zorder=20)
+    ax.text(n3.rights(1).x(), n3.y() + 1, 'ground wire')
+    
+    
+def plot_singleended(ax, opn, ngnd):
+    n1 = ax.node(opn.lefts(0.5))
+    ax.connect((n1, ngnd))
+
+    
+if __name__ == "__main__":
+    s = plot_style()
+
+    fig, ax = plt.subplots(cmsize=(8.0, 5.5))
+    fig.subplots_adjust(nomargins=True)
+    plot_efish(ax, s)
+    eod1, eod2 = plot_eodcircuit(ax)
+    t = ax.text(0.03, 0.03, 'Dipole field of electric fish',
+                transform=ax.transAxes)
+    fig.savefig()
+    ngnd, opn = plot_fishfinder_circuit(ax, s, eod1, eod2)
+    t.set_text('Measurement with fishfinder')
+    fig.savefig('eodcircuit-fishfinder')
+
+    # far ground:
+    plot_farground(ax, eod1, eod2, ngnd)
+    t.set_text('Differential with ground electrode')
+    fig.savefig('eodcircuit-farground')
+
+    # single ended:
+    fig, ax = plt.subplots(cmsize=(8.0, 5.5))
+    fig.subplots_adjust(nomargins=True)
+    plot_efish(ax, s)
+    eod1, eod2 = plot_eodcircuit(ax)
+    ngnd, opn = plot_fishfinder_circuit(ax, s, eod1, eod2)
+    plot_singleended(ax, opn, ngnd)
+    ax.text(0.03, 0.03, 'Single ended',
+            transform=ax.transAxes)
+    fig.savefig('eodcircuit-se')
+    
---
+++ docs/images/plotstyle.py
@@ -0,0 +1,121 @@
+import matplotlib as mpl
+import plottools.plottools as pt
+from plottools.colors import lighter
+from plottools.spines import spines_params
+
+    
+def plot_style():
+    palette = pt.palettes['muted']
+    palette['black'] = pt.palettes['unituebingen']['black']
+    class ns: pass
+    ns.lwthick = 1.8
+    ns.lwthin = 1.0
+    ns.lwspines = 1.0
+    pt.generic_styles(colors='muted', lwthick=ns.lwthick, lwthin=ns.lwthin,
+                      markerlarge=6.5, markersmall=4.0, mec=0.0, mew=0.8,
+                      fillalpha=0.4, namespace=ns)
+    pt.make_line_styles(ns, 'ls', 'Spine', '', palette['black'], '-', ns.lwspines,
+                        clip_on=False)
+    pt.make_line_styles(ns, 'ls', 'Grid', '', palette['gray'], '--', 0.7*ns.lwthin)
+    pt.make_line_styles(ns, 'ls', 'Dotted', '', palette['gray'], ':', 0.7*ns.lwthin)
+    pt.make_line_styles(ns, 'ls', 'Marker', '', palette['black'], '-', ns.lwthick,
+                     clip_on=False)
+    pt.make_line_styles(ns, 'ls', 'Lat', '', palette['lightgreen'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'Diag', '', palette['green'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'Front', '', palette['cyan'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'Lat', 'd', palette['lightgreen'], '--', ns.lwthin)
+    pt.make_line_styles(ns, 'ls', 'Diag', 'd', palette['green'], '--', ns.lwthin)
+    pt.make_line_styles(ns, 'ls', 'Front', 'd', palette['cyan'], '--', ns.lwthin)
+    pt.make_line_styles(ns, 'ls', 'ExpLat', '', palette['blue'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'ExpDiag', '', palette['purple'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'ExpFront', '', palette['orange'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'Visual', '', palette['green'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'Comm', '', palette['blue'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'Navi', '', palette['purple'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'Local', '', palette['red'], '-', ns.lwthick)
+    pt.make_line_styles(ns, 'ls', 'EOD', '', palette['blue'], '-', 0.7)
+    pt.make_line_styles(ns, 'ls', 'EOD2', '', palette['purple'], '-', 0.7)
+    pt.make_line_styles(ns, 'ls', 'Fieldline', '', palette['purple'], '-', 0.7)
+    pt.arrow_style(ns, 'Line', dist=3.0, style='>', shrink=0, lw=0.6,
+                color=palette['black'], head_length=4, head_width=4)
+    pt.arrow_style(ns, 'Hertz', dist=3.0, style='>', shrink=0, lw=0.6,
+                color=palette['black'], head_length=3, head_width=3,
+                heads='<>', text='%.0f\u2009Hz', rotation='vertical', fontsize='x-small',
+                bbox=dict(boxstyle='round,pad=0.1', facecolor='white', edgecolor='none',
+                          alpha=0.6))
+    pt.arrow_style(ns, 'Point', dist=3.0, style='>>', shrink=0, lw=1.2,
+                color=palette['black'], head_length=8, head_width=6)
+    pt.arrow_style(ns, 'PointSmall', dist=3.0, style='>>', shrink=0, lw=1,
+                color=palette['black'], head_length=5, head_width=5,
+                fontsize='small')
+    pt.arrow_style(ns, 'Marker', dist=3.0, style='>>', shrink=0, lw=0.9,
+                color=palette['black'], head_length=5, head_width=5,
+                fontsize='small', ha='center', va='center',
+                bbox=dict(boxstyle='round, pad=0.1, rounding_size=0.4',
+                          facecolor=palette['white'], edgecolor='none', alpha=0.6))
+    ns.tsEOD = dict(fontsize='x-small',
+                    bbox=dict(boxstyle='round,pad=0.1', facecolor='white',
+                              edgecolor='none', alpha=0.6))
+    ns.csLine = dict(colors=palette['gray'], linewidths=0.1, linestyles='solid')
+    ns.fishBody = dict(zorder=21, lw=0.1, edgecolor=palette['black'],
+                       facecolor=palette['black'])
+    ns.fishFins = dict(zorder=20, lw=0.5, alpha=0.4,
+                       edgecolor=pt.lighter(palette['black'], 0.4),
+                       facecolor=palette['black'])
+    ns.fishOpenBody = dict(zorder=21, lw=0.2,
+                           edgecolor=palette['black'], facecolor='none')
+    ns.fishLightBody = dict(zorder=21, lw=0.2, edgecolor=palette['black'],
+                            facecolor=pt.lighter(palette['black'], 0.2))
+    ns.fishLightFins = dict(zorder=20, lw=0.2, edgecolor=palette['black'],
+                            facecolor=pt.lighter(palette['black'], 0.05))
+    ns.fishTransparentFins = dict(zorder=20, lw=0.2, alpha=0.5,
+                                  edgecolor=palette['black'],
+                                  facecolor=lighter(palette['black'], 0.1))
+    ns.fishEyeLine = dict(zorder=22, lw=0.2, edgecolor=palette['black'],
+                          facecolor='none')
+    
+    # rc settings:
+    cmcolors = [palette['red'], pt.lighter(palette['orange'], 0.85),
+                pt.lighter(palette['yellow'], 0.2), pt.lighter(palette['lightblue'], 0.8),
+                palette['blue']]
+    cmvalues = [0.0, 0.25, 0.5, 0.8, 1.0]
+    pt.colormap('RYB', cmcolors, cmvalues)
+    #mpl.rcParams['image.cmap'] = 'RYB'
+    #mpl.rcParams['image.cmap'] = 'RdYlBu'
+    cmcolors = [pt.lighter(palette['yellow'], 0.2), pt.lighter(palette['orange'], 0.85),
+                palette['red']]
+    cmvalues = [0.0, 0.5, 1.0]
+    pt.colormap('YR', cmcolors, cmvalues)
+    cmcolors = [pt.lighter(palette['yellow'], 0.2), palette['green'],
+                palette['darkgreen'], pt.darker(palette['darkgreen'], 0.5)]
+    cmvalues = [0.0, 0.6, 0.8, 1.0]
+    pt.colormap('YG', cmcolors, cmvalues)
+    pt.axes_params(xmargin=0, ymargin=0, zmargin=0, color='none')
+    cycle_colors = ['blue', 'red', 'orange', 'lightgreen', 'magenta', 'yellow', 'cyan', 'pink']
+    pt.circuits_params(scale=3, connectwidth=0.8, linewidth=1.2,
+                       color=palette['black'],
+                       facecolor=palette['white'], alpha=0.5,
+                       zorder=100)
+    pt.colors_params(ns.palette, cycle_colors, cmap='RdYlBu')
+    pt.figure_params(color='none', format='pdf',
+                  compression=6, fonttype=3, stripfonts=False)
+    pt.grid_params(grid=False, axis='both', which='major', **ns.lsGrid)
+    pt.labels_params(labelformat='{label} [{unit}]', labelsize='small', labelweight='normal',
+                  labelcolor='axes', labelpad=4,
+                  xlabellocation='center', ylabellocation='center')
+    pt.legend_params(fontsize='small', frameon=False, borderpad=0,
+                  handlelength=1.5, handletextpad=0.5,
+                  numpoints=1, scatterpoints=1, labelspacing=0.5, columnspacing=0.5)
+    pt.scalebars_params(format_large='%.0f', format_small='%.1f', lw=2.0, color=palette['black'],
+                    capsize=0, clw=0.5, font=dict(fontsize='small', fontstyle='normal'))
+    pt.spines_params(spines='lb', spines_offsets={'lrtb': 3}, spines_bounds={'lrtb': 'full'},
+                  color=ns.lsSpine['color'], linewidth=ns.lsSpine['linewidth'])
+    pt.tag_params(xoffs='auto', yoffs='auto', label='%A', minor_label='%A$_{\text{%mi}}$',
+               font=dict(fontsize='x-large', fontstyle='normal', fontweight='normal'))
+    pt.text_params(font_size=9.0, font_family='sans-serif', color='axes')
+    pt.ticks_params(xtick_minor=False, xtick_dir='out', xtick_size=3.5, minor_tick_frac=0.6,
+                 xtick_major_width=None, xtick_minor_width=None, xtick_major_pad=None,
+                 xtick_alignment='center', ytick_alignment='center_baseline',
+                 xtick_color='axes', xtick_labelcolor='ticks', xtick_labelsize='medium')
+    return ns
+
