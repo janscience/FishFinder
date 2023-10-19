e346cf005cb4f78490ff39d0e95831e7d42f0bb5
Jan Benda <jan.benda@uni-tuebingen.de>
2023-10-19 10:08:21 +0200

    [R42-fishrecorder] random blink pattern

--- R42-fishrecorder/R42-fishrecorder.ino
+++ R42-fishrecorder/R42-fishrecorder.ino
@@ -7,7 +7,6 @@
 #include <Configurator.h>
 #include <Settings.h>
 #include <InputTDMSettings.h>
-#include <FileStorage.h>
 
 // Default settings: ----------------------------------------------------------
 // (may be overwritten by config file logger.cfg)
@@ -43,6 +42,147 @@ RTClock rtclock;
 Blink blink(LED_PIN);
 
 
+int restarts = 0;
+String prevname; // previous file name
+
+
+void setupStorage(char *gainstr) {
+  prevname = "";
+  restarts = 0;
+  blink.setTiming(5000, 60, 1000);
+  if (file.sdcard()->dataDir(settings.Path))
+    Serial.printf("Save recorded data in folder \"%s\".\n\n", settings.Path);
+  file.setWriteInterval(2*aidata.DMABufferTime());
+  file.setMaxFileTime(settings.FileTime);
+  file.header().setSoftware(SOFTWARE);
+  if (gainstr != 0)
+    file.header().setGain(gainstr);
+  file.start();
+}
+
+
+void openNextFile() {
+  blink.setRandom();
+  blink.blinkSingle(0, 2000);
+  time_t t = now();
+  String fname = rtclock.makeStr(settings.FileName, t, true);
+  if (fname != prevname) {
+    file.sdcard()->resetFileCounter();
+    prevname = fname;
+  }
+  fname = file.sdcard()->incrementFileName(fname);
+  if (fname.length() == 0) {
+    blink.clear();
+    Serial.println("WARNING: failed to increment file name.");
+    Serial.println("SD card probably not inserted.");
+    Serial.println();
+    aidata.stop();
+    while (1) { yield(); };
+    return;
+  }
+  char dts[20];
+  rtclock.dateTime(dts, t);
+  if (! file.openWave(fname.c_str(), -1, dts)) {
+    blink.clear();
+    Serial.println();
+    Serial.println("WARNING: failed to open file on SD card.");
+    Serial.println("SD card probably not inserted or full -> halt");
+    aidata.stop();
+    while (1) { yield(); };
+    return;
+  }
+  ssize_t samples = file.write();
+  if (samples == -4) {   // overrun
+    file.start(aidata.nbuffer()/2);   // skip half a buffer
+    file.write();                     // write all available data
+    // report overrun:
+    char mfs[100];
+    sprintf(mfs, "%s-error0-overrun.msg", file.baseName().c_str());
+    Serial.println(mfs);
+    File mf = sdcard.openWrite(mfs);
+    mf.close();
+  }
+  Serial.println(file.name());
+}
+
+
+void storeData() {
+  if (!file.pending())
+    return;
+  ssize_t samples = file.write();
+  if (samples < 0) {
+    blink.clear();
+    Serial.println();
+    Serial.println("ERROR in writing data to file:");
+    char errorstr[20];
+    switch (samples) {
+      case -1:
+        Serial.println("  File not open.");
+        break;
+      case -2:
+        Serial.println("  File already full.");
+        break;
+      case -3:
+        aidata.stop();
+        Serial.println("  No data available, data acquisition probably not running.");
+        Serial.printf("  dmabuffertime = %.2fms, writetime = %.2fms\n", 1000.0*aidata.DMABufferTime(), 1000.0*file.writeTime());
+        strcpy(errorstr, "nodata");
+        delay(20);
+        break;
+      case -4:
+        Serial.println("  Buffer overrun.");
+        strcpy(errorstr, "overrun");
+        break;
+      case -5:
+        Serial.println("  Nothing written into the file.");
+        Serial.println("  SD card probably full -> halt");
+        aidata.stop();
+        while (1) {};
+        break;
+    }
+    if (samples <= -3) {
+      file.closeWave();
+      char mfs[100];
+      sprintf(mfs, "%s-error%d-%s.msg", file.baseName().c_str(), restarts+1, errorstr);
+      Serial.println(mfs);
+      File mf = sdcard.openWrite(mfs);
+      mf.close();
+      Serial.println();
+    }
+  }
+  if (file.endWrite() || samples < 0) {
+    file.close();  // file size was set by openWave()
+#ifdef SINGLE_FILE_MTP
+    aidata.stop();
+    delay(50);
+    Serial.println();
+    Serial.println("MTP file transfer.");
+    Serial.flush();
+    blink.setTriple();
+    MTP.begin();
+    MTP.addFilesystem(sdcard, "logger");
+    while (true) {
+      MTP.loop();
+      blink.update();
+      yield();
+    }
+#endif      
+    if (samples < 0) {
+      restarts++;
+      if (restarts >= 5) {
+        Serial.println("ERROR: Too many file errors -> halt.");
+        aidata.stop();
+        while (1) { yield(); };
+      }
+      if (!aidata.running())
+        aidata.start();
+      file.start();
+    }
+    openNextFile();
+  }
+}
+
+
 bool setupPCM(InputTDM &tdm, ControlPCM186x &pcm) {
   pcm.begin();
   bool r = pcm.setMicBias(false, true);
@@ -52,7 +192,7 @@ bool setupPCM(InputTDM &tdm, ControlPCM186x &pcm) {
   }
   pcm.setRate(tdm, aisettings.rate());
   pcm.setupTDM(tdm, ControlPCM186x::CH2L, ControlPCM186x::CH2R,
-               offs, ControlPCM186x::INVERTED);
+               false, ControlPCM186x::INVERTED);
   Serial.println("configured for 2 channels");
   pcm.setSmoothGainChange(false);
   pcm.setGain(aisettings.gain());
@@ -93,8 +233,8 @@ void setup() {
   else
     delay(uint32_t(1000.0*settings.InitialDelay));
   char gs[16];
-  pcm->gainStr(gs, PREGAIN);
-  setupStorage(SOFTWARE, aidata, gs);
+  pcm1.gainStr(gs, PREGAIN);  // XXX only from channel one!
+  setupStorage(gs);
   openNextFile();
 }
 
